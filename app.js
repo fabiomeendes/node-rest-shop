ac46551238cab51ef33a689723232e06d0c5a37e
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-06-27 15:44:47 -0300

    Install mongoose and add url db atlas

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASS}@node-rest-shop-i4p7x.mongodb.net/test?retryWrites=true&w=majority`);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
