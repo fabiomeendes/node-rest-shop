ecceea7a17a5e80ea08680b920a186d66f5eaaaf
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-10 19:30:34 -0300

    Login route

--- api/routes/users.js
+++ api/routes/users.js
@@ -42,6 +42,33 @@ router.post('/signup', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: 'Auth failed!',
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: 'Auth failed',
+          });
+        }
+        if (result) {
+          return res.status(401).json({
+            message: 'Auth successful',
+          });
+        }
+        res.status(401).json({
+          message: 'Auth failed!!',
+        });
+      });
+    });
+});
+
 router.get('/', (req, res) => {
   User
     .find()
